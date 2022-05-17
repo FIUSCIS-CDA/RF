59df39e9b7d555f003c7f39aa81fe8f81b1dc18f
movingpictures83 <movingpictures83@gmail.com>
2022-05-17 09:54:03 -0400

    Added color New testbench format

--- RF.v
+++ RF.v
@@ -1,25 +1,25 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
-// and other software and tools, and its AMPP partner logic 
+// Copyright (C) 2020  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and any partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details, at
+// https://fpgasoftware.intel.com/eula.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Tue Feb  2 07:18:40 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
+// CREATED		"Thu May 12 18:03:54 2022"
 
 module RF(
-	clk,
 	reset,
+	clk,
 	we,
 	r1a,
 	r2a,
@@ -30,8 +30,8 @@ module RF(
 );
 
 
-input wire	clk;
 input wire	reset;
+input wire	clk;
 input wire	we;
 input wire	[4:0] r1a;
 input wire	[4:0] r2a;
--- testbench.v
+++ testbench.v
@@ -1,42 +1,69 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Testbench for Component: RF (CLK=20)
+// Package: FIUSCIS-CDA
+// Course: CDA3102 (Computer Architecture), Florida International University
+// Developer: Trevor Cickovski
+// License: MIT, (C) 2020 All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////////
 
 module testbench();
+`include "../Test/Test.v"
 
+///////////////////////////////////////////////////////////////////////////////////
+// Inputs: clk, reset, we (1-bit), D (32-bit)
+//         r1a, r2a, wa (5-bit)
+//         wd (32-bit)
 reg clk, reset; 
 reg we;
 reg[4:0] r1a;
 reg[4:0] r2a;
 reg[4:0] wa;
 reg[31:0] wd;
+///////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+// Outputs: r1d, r2d (32-bit)
 wire[31:0] r1d;
 wire[31:0] r2d;
+///////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////////////
+// Component is CLOCKED
+// Set clk period to 20 in wave
 localparam CLK_PERIOD=20;
+///////////////////////////////////////////////////////////////////////////////////
 
-always @*
-begin
-   clk <= 1;  # (CLK_PERIOD/2);
-   clk <= 0;  # (CLK_PERIOD/2);
-end
-
-
-RF myRegister(clk, reset, we, r1a, r2a, wa, wd, r1d, r2d);
+RF myRegister(.clk(clk), .reset(reset), .we(we), .r1a(r1a), .r2a(r2a), .wa(wa), 
+              .wd(wd), .r1d(r1d), .r2d(r2d));
+integer stop;
 
 initial begin
+
+/////////////////////////////////////////////////////////////////////////////
+// Test: reset=1
+$display("Testing reset: All Registers=0");
 reset=1;  #(CLK_PERIOD);
+stop=0;
+for (wa = 5'b00000; (stop == 0); wa = wa + 5'b00001) begin
+   verifyEqual32(r1d, 0);
+   verifyEqual32(r2d, 0);
+   $display("Testing %b=0", wa);
+   if (wa == 5'b11111) begin
+      stop = 1;
+   end
+end
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+// Test: write enable (one on, one off)
+$display("Testing write enable (r1d=0, r2d=45)");
 reset=0; we=0; wa=5'b10011; wd=121; #(CLK_PERIOD);  
 we=1; wa=5'b01101; wd=45; #(CLK_PERIOD);
 r1a=5'b10011; r2a=5'b01101; #(CLK_PERIOD);
+verifyEqual32(r1d, 0);
+verifyEqual32(r2d, 45);
+/////////////////////////////////////////////////////////////////////////////
 
-if (r1d === 121) begin
-   $display("Error: Register r1a written with zero enable signal"); $stop;
-end
-if (r2d !== 45) begin
-   $display("Error: Register r2a not reading properly, or not written with enable signal"); $stop;
-end
-r1a=5'b01101; #(CLK_PERIOD);
-if (r1d === 121) begin
-   $display("Error: Register r1a not reading properly."); $stop;
-end
 $display("All tests passed.");
 end
 
